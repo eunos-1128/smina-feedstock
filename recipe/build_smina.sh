f45789a2279a58c5df4bedba2e0e0576dc003fab
UENO, M. <eunosium-1128@live.jp>
2025-07-12 02:37:51 +0900

    Update build_smina.sh

--- recipe/build_smina.sh
+++ recipe/build_smina.sh
@@ -10,6 +10,8 @@ echo "const char* GIT_REV=\"${GIT_HASH}\";"    >  version.cpp
 echo "const char* GIT_TAG=\"${PKG_VERSION}\";" >> version.cpp
 echo "const char* GIT_BRANCH=\"conda-forge\";" >> version.cpp
 
+sed -i.bak '/project(smina)/a find_package(Threads REQUIRED)' CMakeLists.txt
+
 cmake ${SRC_DIR} ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
